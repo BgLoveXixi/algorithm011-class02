fe92c674acb8e31f52cbf41be937f4b401ba1d48
Jerry Tom <717562756@qq.com>
2020-07-19 23:48:17 +0800

    homework4-v1

---
+++ Week_04/homework.py
@@ -0,0 +1,122 @@
+##NO.1 搜索旋转排序数组
+# 假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+# ( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。
+# 搜索一个给定的目标值，如果数组中存在这个目标值，则返回它的索引，否则返回 -1 。
+# 你可以假设数组中不存在重复的元素。
+# 你的算法时间复杂度必须是 O(log n) 级别。
+
+# 示例 1:
+
+# 输入: nums = [4,5,6,7,0,1,2], target = 0
+# 输出: 4
+# 示例 2:
+
+# 输入: nums = [4,5,6,7,0,1,2], target = 3
+# 输出: -1
+
+
+class Solution(object):
+    def lemonadeChange(self, bills):
+        """
+        :type bills: List[int]
+        :rtype: bool
+        """
+        five = ten = 0
+        for bill in bills:
+            if bill == 5:
+                five += 1
+            elif bill == 10:
+                if not five: return False
+                five -= 1
+                ten += 1
+            else:
+                if ten and five:
+                    ten -= 1
+                    five -= 1
+                elif five >= 3:
+                    five -= 3
+                else:
+                    return False
+        return True
+
+
+
+##NO.2  模拟行走机器人
+# 机器人在一个无限大小的网格上行走，从点 (0, 0) 处开始出发，面向北方。该机器人可以接收以下三种类型的命令：
+
+# -2：向左转 90 度
+# -1：向右转 90 度
+# 1 <= x <= 9：向前移动 x 个单位长度
+# 在网格上有一些格子被视为障碍物。
+# 第 i 个障碍物位于网格点  (obstacles[i][0], obstacles[i][1])
+# 机器人无法走到障碍物上，它将会停留在障碍物的前一个网格方块上，但仍然可以继续该路线的其余部分。
+# 返回从原点到机器人所有经过的路径点（坐标为整数）的最大欧式距离的平方。
+
+# 示例 1：
+# 输入: commands = [4,-1,3], obstacles = []
+# 输出: 25
+# 解释: 机器人将会到达 (3, 4)
+
+# 示例 2：
+# 输入: commands = [4,-1,4,-2,4], obstacles = [[2,4]]
+# 输出: 65
+# 解释: 机器人在左转走到 (1, 8) 之前将被困在 (1, 4) 处
+
+
+class Solution(object):
+    def robotSim(self, commands, obstacles):
+        """
+        :type commands: List[int]
+        :type obstacles: List[List[int]]
+        :rtype: int
+        """
+        dx = [0, 1, 0, -1]
+        dy = [1, 0, -1, 0]
+        x = y = di = 0
+        obstacleSet = set(map(tuple, obstacles))
+        ans = 0
+
+        for cmd in commands:
+            if cmd == -2:  #left
+                di = (di - 1) % 4
+            elif cmd == -1:  #right
+                di = (di + 1) % 4
+            else:
+                for k in xrange(cmd):
+                    if (x+dx[di], y+dy[di]) not in obstacleSet:
+                        x += dx[di]
+                        y += dy[di]
+                        ans = max(ans, x*x + y*y)
+
+        return ans
+
+
+##NO.3  寻找旋转排序数组中的最小值
+# 假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+# ( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。
+# 请找出其中最小的元素。
+
+# 你可以假设数组中不存在重复元素。
+
+# 示例 1:
+# 输入: [3,4,5,1,2]
+# 输出: 1
+
+# 示例 2:
+# 输入: [4,5,6,7,0,1,2]
+# 输出: 0
+
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        left, right = 0, len(nums) - 1
+        while left < right:
+            mid = (left + right) >> 1
+            if nums[mid] > nums[right]:         
+                left = mid + 1
+            else:                               
+                right = mid
+        return nums[left]
